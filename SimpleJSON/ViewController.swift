138c6040505def8c9ed37f5584e8aaf3c7ff91c5
fxdlsRider <zx501@icloud.com>
2020-01-28 18:43:53 +0100

    Initial Commit

---
+++ SimpleJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleJSON
+//
+//  Created by Zoran Zdravkovic on 28.01.20.
+//  Copyright © 2020 Zoran Zdravkovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
